17a4c7cc524fc700156d08e650bf9cc765d647a3
Veerendra Srivastava <veerendra118@gmail.com>
2026-05-28 04:17:29 +0530

    add two basic java file

---
+++ ArrayDemo.java
@@ -0,0 +1,13 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Arrays Example");
+        int arr[]={10,2};
+        int [] arr1;
+        //int [6] arr2; error: not a statement
+        System.out.println(arr.length);
+        int newarr[]=new int[10];
+        System.out.println(newarr.length);
+
+       // int newarr1[]=new int[];error: array dimension missing
+    }
+}
